20052720c1f926116ad43f1952e5281760f84258
24110159-pbao <24110159@student.hcmute.edu.vn>
2026-08-13 18:24:01 +0700

    add wrong

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello CI/CD"}
+    assert response.json() == {"message": "Hello CI/CD huhu"}
 
 
 def test_health():
