6542afb14c44c87486c78b25526f979edd987f91
24110159-pbao <24110159@student.hcmute.edu.vn>
2026-08-13 20:45:01 +0700

    add /not-found

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,3 +16,8 @@ def test_health():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+def test_not_found():
+    response = client.get("/not-found")
+    assert response.status_code == 200
+    assert response.json() == {"detail": "Not Found"}
